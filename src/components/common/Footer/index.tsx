3bdbdc8e4c9877eda4ffb0d50ebce83b78799d58
keso-12 <kenneth@xtendops.us>
2021-08-25 23:27:00 +0800

    feat: revamped navbar added footer

---
+++ src/components/common/Footer/index.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import {
+  Paper, Grid, Typography, IconButton,
+} from '@material-ui/core';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import { useSnackbar } from 'notistack';
+
+const useStyles = makeStyles((theme) => createStyles({
+  root: {
+    marginTop: '2em',
+    padding: '1em 2em',
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    color: 'red',
+  },
+}));
+
+const Footer = () => {
+  const classes = useStyles();
+  const { enqueueSnackbar } = useSnackbar();
+
+  const copyToClipboard = (text: string) => {
+    navigator.clipboard.writeText(text);
+    enqueueSnackbar('This is a success message!', { variant: 'success' });
+  };
+
+  const bsc = '0x0586566A17125051792b66c9d3f1f8917db2DE87';
+  const eth = '0x7CED965f78451Ba8e33520DC2f9e322EF537F5CB';
+  const paypal = 'https://www.paypal.com/paypalme/my/profile';
+
+  return (
+    <Paper className={classes.root}>
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography>Like my work? Buy me a coffee.</Typography>
+          <Typography>
+            BSC Address:
+            {' '}
+            {bsc}
+            <IconButton color="primary" aria-label="upload picture" component="span" onClick={() => copyToClipboard(bsc)}>
+              <FileCopyIcon />
+            </IconButton>
+          </Typography>
+          <Typography>
+            ETH Address:
+            {' '}
+            {eth}
+            <IconButton color="primary" aria-label="upload picture" component="span" onClick={() => copyToClipboard(eth)}>
+              <FileCopyIcon />
+            </IconButton>
+          </Typography>
+          <Typography>
+            <a href={paypal}>Paypal</a>
+          </Typography>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
+
+export default Footer;
--- src/components/common/NavBar/index.tsx
+++ src/components/common/NavBar/index.tsx
@@ -1,41 +1,62 @@
 import React from 'react';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import {
-  AppBar, Toolbar, Typography, Button,
+  AppBar, Toolbar, Typography, Box, IconButton,
 } from '@material-ui/core';
+import Brightness4Icon from '@material-ui/icons/Brightness4';
+import DragonaryLogo from 'assets/Dragonary/logo_dragonary.png';
 import Link from '../../shared/Link';
 
 const useStyles = makeStyles((theme) => createStyles({
   root: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
   title: {
     flexGrow: 1,
-    color: 'red',
+    color: 'inherit',
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    width: '100%',
+  },
+  item: {
+    margin: '0 0.4em',
+  },
+  logo: {
+    width: '6%',
   },
 }));
 
-const NavBar = () => {
+const NavBar = ({ toggleTheme }: { toggleTheme: () => void }) => {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <AppBar position="fixed">
-        <Toolbar>
-          <Link path="/">
-            <Typography variant="h6" className={classes.title}>
-              APP
-            </Typography>
-          </Link>
-          <Link path="/login">
-            <Button color="inherit">Login</Button>
-          </Link>
+        <Toolbar variant="dense">
+          <Box className={classes.container}>
+            <Box className={classes.logo}><Typography variant="h4">KS</Typography></Box>
+            <Box className={classes.item}>
+              <Link path="/">
+                <Typography variant="h6" className={classes.title}>
+                  Home
+                </Typography>
+              </Link>
+            </Box>
+            <Box className={classes.item}>
+              <Link path="/dragonary">
+                <img src={DragonaryLogo} alt="Dragonary" style={{ width: '140px' }} />
+              </Link>
+            </Box>
+            <Box className={classes.item} style={{ marginLeft: 'auto' }}>
+              <IconButton onClick={toggleTheme}>
+                <Brightness4Icon />
+              </IconButton>
+            </Box>
+          </Box>
         </Toolbar>
       </AppBar>
-      <Toolbar />
     </div>
   );
 };
