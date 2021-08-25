a22a029047fef0cc64d44cf8140fbbf9ae2ebf67
keso-12 <kenneth@xtendops.us>
2021-08-25 23:28:43 +0800

    refactor: changed home display

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,29 +1,8 @@
 import React from 'react';
-import Link from '../shared/Link';
-import './App.css';
-import { Button } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Home
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Link path="/login">
-          <Button variant="contained">Login</Button>
-        </Link>
-      </header>
-    </div>
+    <div />
   );
 }
 
